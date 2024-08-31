1c9b295ee23b7f198218809f72f026f0c0c6eb81
Alexander <alexander.bekkerin@zigt.nl>
2024-08-31 23:24:28 +0200

    rabbitmq rewrite

--- index.js
+++ index.js
@@ -1,117 +1,104 @@
 require('dotenv').config()
+
 const axios = require('axios')
-const { google } = require('googleapis')
 const mysql = require('mysql2/promise')
-const credentials =
-	process.env.NODE_ENV === 'development'
-		? require('./credentials-dev.json')
-		: require('./credentials.json')
-const path = require('path')
+const amqp = require('amqplib/callback_api')
+
+const username = process.env.RABBITMQ_USER
+const password = process.env.RABBITMQ_PASS
+const host = process.env.RABBITMQ_HOST
+const connectionString = `amqps://${username}:${password}@${host}`
+
 const webhookUrl = process.env.WEBHOOK_URL
 
-let totalUsers = [];
-totalUsers['new'] = 0;
-totalUsers['updated'] = 0;
-let unknowServers = ''
-let startTime = performance.now()
+let connection = false
+let serverDoesntExist = false
+let totalUsers = { new: 0, updated: 0 }
 
+let _guildId
+let oldGuildId
+let inactivityTimer = null
+let scanDoneTimer = null
+let wasDone = false
 
-async function authenticate() {
-	const auth = new google.auth.GoogleAuth({
-		credentials,
-		scopes: ['https://www.googleapis.com/auth/drive'],
-	})
-	return auth.getClient()
-}
-
-
-function millisToMinutesAndSeconds(millis) {
-	var minutes = Math.floor(millis / 60000);
-	var seconds = ((millis % 60000) / 1000).toFixed(0);
-	return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-  }
-async function sendDiscordWebhookMessage(description, color = 0x00ff00) {
+const sendDiscordWebhookMessage = async (description, color = 0x00ff00) => {
 	try {
-		const response = await axios.post(webhookUrl, {
+		await axios.post(webhookUrl, {
 			embeds: [
 				{
-					title: 'Import',
 					description: description,
 					color: color,
 				},
 			],
 		})
-		console.log('Message sent successfully:', response.data)
 	} catch (error) {
 		console.error('Error sending message:', error)
 	}
 }
 
-async function listAndReadJsonFiles(auth) {
-	const drive = google.drive({ version: 'v3', auth })
-	let pageToken = null
-	do {
-		try {
-			const res = await drive.files.list({
-				q: "mimeType='application/json'",
-				pageSize: 100,
-				fields: 'nextPageToken, files(id, name)',
-				pageToken: pageToken,
-			})
-			const files = res.data.files
-			if (files.length) {
-				console.log('Files:')
-				for (const file of files) {
-					console.log(`${file.name} (${file.id})`)
-					await readFileContent(drive, file.id, file.name)
-					await deleteFile(drive, file.id)
-				}
-				let endTime = performance.now()
-				await sendDiscordWebhookMessage(
-					`All **${files.length}** file(s) have been processed. Total New users:**${totalUsers['new']}** / Total updated users:**${totalUsers['updated']}**  / Total Time taken: **${millisToMinutesAndSeconds(endTime - startTime)}** / Servers not found: ${unknowServers}`
-				)
-			} else {
-				console.log('No files found.')
-				await sendDiscordWebhookMessage(`No files found to import.`, 0xff0000)
-			}
-			pageToken = res.data.nextPageToken
-		} catch (error) {
-			console.error('Error listing files:', error)
-			break
-		}
-	} while (pageToken)
-}
-
-async function readFileContent(drive, fileId, fileName) {
-	try {
-		const res = await drive.files.get(
-			{ fileId, alt: 'media' },
-			{ responseType: 'stream' }
-		)
-		let data = ''
-		res.data.on('data', (chunk) => {
-			data += chunk
+const getConnection = async () => {
+	if (!connection || connection.connection._closing) {
+		connection = await mysql.createConnection({
+			host: process.env.DB_HOST,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
 		})
-		res.data.on('end', async () => {
-			const jsonData = JSON.parse(data)
-			await processJsonData(jsonData, fileName)
-		})
-	} catch (error) {
-		console.error(`Error reading file ${fileName}:`, error)
 	}
+	return connection
 }
 
-async function processJsonData(jsonData, fileName) {
-	const connection = await mysql.createConnection({
-		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
-	})
+const resetScanDoneTimer = () => {
+	if (scanDoneTimer) {
+		clearTimeout(scanDoneTimer)
+	}
+	scanDoneTimer = setTimeout(async () => {
+		await sendDiscordWebhookMessage(
+			`Import done for **${_guildId}**\n New users: **${totalUsers['new']}**\n Updated users: **${totalUsers['updated']}**`
+		)
 
+		console.log('Guild done, resetting')
+
+		serverDoesntExist = false
+		totalUsers = { new: 0, updated: 0 }
+		wasDone = true
+	}, 1.5 * 60 * 1000)
+}
+
+const guildIdHandler = {
+	get: () => {
+		return _guildId
+	},
+	set: async (newValue) => {
+		oldGuildId = _guildId
+		_guildId = newValue
+
+		resetScanDoneTimer()
+
+		if (oldGuildId !== undefined && oldGuildId !== _guildId) {
+			if (wasDone) {
+				wasDone = false
+			} else {
+				await sendDiscordWebhookMessage(
+					`Import done for **${oldGuildId}**\n New users: **${totalUsers['new']}**\n Updated users: **${totalUsers['updated']}**`
+				)
+	
+				console.log('Guild done, resetting')
+	
+				serverDoesntExist = false
+				totalUsers = { new: 0, updated: 0 }
+			}
+		}
+	},
+}
+
+Object.defineProperty(global, 'guildId', guildIdHandler)
+
+const processUser = async (serverId, user) => {
 	const typeHierarchy = ['OTHER', 'LEAKER', 'CHEATER', 'SUPPORTER', 'OWNER']
 
-	const serverId = path.basename(fileName, '.json').split('-')[1]
+	let added = 0
+	let updated = 0
 
 	const [serverRows] = await connection.execute(
 		'SELECT id FROM BadServers WHERE id = ?',
@@ -119,119 +106,151 @@ async function processJsonData(jsonData, fileName) {
 	)
 
 	if (serverRows.length === 0) {
-		console.error(`Server ID ${serverId} does not exist in the BadServers table.`)
-		unknowServers += ` / ${serverId}`;
 		await sendDiscordWebhookMessage(
-			`Server ID **${serverId}** wasn't found in the bad servers list.`,
+			`${serverId} doesn't exist in the database`,
 			0xff0000
 		)
-		await connection.end()
+		serverDoesntExist = true
 		return
 	}
 
-	await sendDiscordWebhookMessage(
-		`Importing **${jsonData.length}** users for **${serverId}**...`
+	const { id, type, roles = [] } = user
+
+	if (!id || !type) {
+		return
+	}
+
+	const [rows] = await connection.execute(
+		'SELECT id, type, status FROM Users WHERE id = ?',
+		[id]
 	)
-	let added = 0
-	let updated = 0
 
-	for (const entry of jsonData) {
-		const { id, type, roles = [] } = entry
+	let status = 'BLACKLISTED'
+	if (type === 'SUPPORTER' || type === 'OWNER') {
+		status = 'PERM_BLACKLISTED'
+	}
 
-		if (!id || !type) {
-			continue
+	if (rows.length > 0) {
+		const currentType = rows[0].type
+		const currentStatus = rows[0].status
+
+		if (currentStatus === 'WHITELISTED') {
+			console.log(`User ${id} is whitelisted, skipping.`)
+			return
 		}
 
-		const [rows] = await connection.execute(
-			'SELECT id, type, status FROM Users WHERE id = ?',
-			[id]
-		)
-
-		let status = 'BLACKLISTED'
-		if (type === 'SUPPORTER' || type === 'OWNER') {
+		if (currentStatus === 'PERM_BLACKLISTED') {
 			status = 'PERM_BLACKLISTED'
 		}
 
-		if (rows.length > 0) {
-			const currentType = rows[0].type
-			const currentStatus = rows[0].status
+		const currentTypeIndex = typeHierarchy.indexOf(currentType)
+		const newTypeIndex = typeHierarchy.indexOf(type)
 
-			if (currentStatus === 'WHITELISTED') {
-				console.log(`User ${id} is whitelisted, skipping.`)
-				continue
-			}
-
-			const currentTypeIndex = typeHierarchy.indexOf(currentType)
-			const newTypeIndex = typeHierarchy.indexOf(type)
-
-			if (newTypeIndex > currentTypeIndex) {
-				await connection.execute(
-					'UPDATE Users SET type = ?, status = ? WHERE id = ?',
-					[type, status, id]
-				)
-			} else {
-				await connection.execute('UPDATE Users SET status = ? WHERE id = ?', [
-					status,
-					id,
-				])
-			}
-		} else {
+		if (newTypeIndex > currentTypeIndex) {
 			await connection.execute(
-				'INSERT INTO Users (id, last_username, avatar, type, status) VALUES (?, ?, ?, ?, ?)',
-				[id, 'EMPTY', 'https://cdn.discordapp.com/embed/avatars/0.png', type, status]
+				'UPDATE Users SET type = ?, status = ? WHERE id = ?',
+				[type, status, id]
 			)
+		} else {
+			await connection.execute('UPDATE Users SET status = ? WHERE id = ?', [
+				status,
+				id,
+			])
 		}
-
-		const rolesString = roles.length > 0 ? roles.join(', ') : ''
-
-		const [importRows] = await connection.execute(
-			'SELECT id, type FROM Imports WHERE id = ? AND server = ?',
-			[id, serverId]
+	} else {
+		await connection.execute(
+			'INSERT INTO Users (id, last_username, avatar, type, status) VALUES (?, ?, ?, ?, ?)',
+			[id, 'EMPTY', 'https://cdn.discordapp.com/embed/avatars/0.png', type, status]
 		)
+	}
 
-		if (importRows.length > 0) {
-			const currentType = importRows[0].type
-			const currentTypeIndex = typeHierarchy.indexOf(currentType)
-			const newTypeIndex = typeHierarchy.indexOf(type)
+	const rolesString = roles.length > 0 ? roles.join(', ') : ''
 
-			if (newTypeIndex > currentTypeIndex) {
-				await connection.execute(
-					'UPDATE Imports SET type = ?, roles = ?, updatedAt = NOW(), appealed = 0 WHERE id = ? AND server = ?',
-					[type, rolesString, id, serverId]
-				)
-				updated = updated + 1
-			} else {
-				await connection.execute(
-					'UPDATE Imports SET roles = ?, appealed = 0, updatedAt = NOW() WHERE id = ? AND server = ?',
-					[rolesString, id, serverId]
-				)
-				updated = updated + 1
-			}
+	const [importRows] = await connection.execute(
+		'SELECT id, type FROM Imports WHERE id = ? AND server = ?',
+		[id, serverId]
+	)
+
+	if (importRows.length > 0) {
+		const currentType = importRows[0].type
+		const currentTypeIndex = typeHierarchy.indexOf(currentType)
+		const newTypeIndex = typeHierarchy.indexOf(type)
+
+		if (newTypeIndex > currentTypeIndex) {
+			await connection.execute(
+				'UPDATE Imports SET type = ?, roles = ?, updatedAt = NOW(), appealed = 0 WHERE id = ? AND server = ?',
+				[type, rolesString, id, serverId]
+			)
+			updated = updated + 1
 		} else {
 			await connection.execute(
-				'INSERT INTO Imports (id, server, roles, type, appealed, createdAt, updatedAt, reason) VALUES (?, ?, ?, ?, ?, NOW(), NOW(), ?)',
-				[id, serverId, rolesString, type, 0, '']
+				'UPDATE Imports SET roles = ?, appealed = 0, updatedAt = NOW() WHERE id = ? AND server = ?',
+				[rolesString, id, serverId]
 			)
-			added = added + 1
+			updated = updated + 1
 		}
+	} else {
+		await connection.execute(
+			'INSERT INTO Imports (id, server, roles, type, appealed, createdAt, updatedAt, reason) VALUES (?, ?, ?, ?, ?, NOW(), NOW(), ?)',
+			[id, serverId, rolesString, type, 0, '']
+		)
+		added = added + 1
 	}
-	totalUsers['new'] += added;
-	totalUsers['updated'] += updated;
-	await sendDiscordWebhookMessage(
-		`Imported **${added}** new users and updated **${updated}** users for **${serverId}**.`
-	)
-	await connection.end()
+
+	totalUsers['new'] += added
+	totalUsers['updated'] += updated
 }
 
-async function deleteFile(drive, fileId) {
-	try {
-		await drive.files.delete({ fileId })
-		console.log(`File ${fileId} deleted successfully.`)
-	} catch (error) {
-		console.error(`Error deleting file ${fileId}:`, error)
+const resetInactivityTimer = () => {
+	if (inactivityTimer) {
+		clearTimeout(inactivityTimer)
 	}
+	inactivityTimer = setTimeout(async () => {
+		if (connection) {
+			await connection.end()
+
+			connection = false
+			serverDoesntExist = false
+			totalUsers = { new: 0, updated: 0 }
+
+			console.log('Database connection closed due to inactivity')
+		}
+	}, 5 * 60 * 1000)
 }
 
-authenticate()
-	.then((auth) => listAndReadJsonFiles(auth))
-	.catch(console.error)
+amqp.connect(connectionString, (error0, connection) => {
+	if (error0) {
+		throw error0
+	}
+	connection.createChannel(async (error1, channel) => {
+		if (error1) {
+			throw error1
+		}
+
+		var queue = 'users'
+
+		channel.assertQueue(queue, {
+			durable: false,
+		})
+
+		console.log('Waiting for guild and users')
+		await sendDiscordWebhookMessage(`Ready to import users`)
+
+		channel.consume(
+			queue,
+			async (msg) => {
+				connection = await getConnection()
+				resetInactivityTimer()
+
+				const item = JSON.parse(msg.content)
+				guildId = item.guildId
+				if (item.id && !serverDoesntExist) await processUser(item.guildId, item)
+
+				console.log(item)
+			},
+			{
+				noAck: true,
+			}
+		)
+	})
+})
